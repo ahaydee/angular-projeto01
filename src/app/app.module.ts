0da64f8f0355e3ab742f8a17c36ce4a339e2f84c
ahaydee <annahaydee01@gmail.com>
2020-09-12 16:16:59 -0300

    Criação dos componentes e adição das rotas no app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+import { DisciplinasDetailsComponent } from './disciplinas-details/disciplinas-details.component';
+import { SemestresComponent } from './semestres/semestres.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent}
+      {path: '', component: HomeComponent},      
+      {path: 'semestres', component: SemestresComponent},      
+      {path: 'semestres/:semestre', component: DisciplinasComponent},
+      {path: 'semestres/:semestre/:disciplina', component: DisciplinasDetailsComponent}
     ])
   ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, DisciplinasComponent, DisciplinasDetailsComponent, SemestresComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/disciplinas-details/disciplinas-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disciplinas-details',
+  templateUrl: './disciplinas-details.component.html',
+  styleUrls: ['./disciplinas-details.component.css']
+})
+export class DisciplinasDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

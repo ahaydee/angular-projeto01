b70f5c9b08dfaa15e3cd75119aaee5babcb07a29
ahaydee <annahaydee01@gmail.com>
2020-09-12 18:45:03 -0300

    Métodos para pegar a quantidade de disciplinas e a média geral

--- src/app/media-geral/media-geral.component.ts
+++ src/app/media-geral/media-geral.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { semestres } from '../consts';
 
 @Component({
   selector: 'app-media-geral',
@@ -6,8 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-geral.component.css']
 })
 export class MediaGeralComponent implements OnInit {
+  list = semestres;
+  notas;
 
-  constructor() { }
+  getNotas(nome: string) {
+    let qtde = 0;
+    let notas: number[] = [];
+    this.list.forEach(semestre => {
+      semestre.disciplinas.forEach(disciplinas => {
+        disciplinas.notas.forEach(nota =>{
+          if (nota.situacao != "A Cursar") {
+            if (nota.nome == nome) {
+              qtde++;
+              notas.push(nota.nota);
+            }
+          }
+        });
+      });
+    });
+    return {
+      qtde: qtde,
+      notas: notas
+    }
+  }
+
+  getMediaAnna() {
+    return this.notas["anna"].notas / this.notas["anna"].qtde;
+  }
+
+  getMediaDanubia() {
+    return this.notas["danubia"].notas / this.notas["danubia"].qtde;
+  }
+
+  constructor() { 
+    this.notas["anna"] = this.getNotas("anna");
+    this.notas["danubia"] = this.getNotas("danubia");
+  }
 
   ngOnInit() {
   }
